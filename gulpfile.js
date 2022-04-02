2f7ee0b79e810591557e200f3af5e0575dce6e3e
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-04-02 10:23:07 +0200

    chore: update gulpfile to create type definitions
    #59

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
 const del = require("del");
+const merge = require("merge2");
 
 const tsProject = ts.createProject("tsconfig.json");
+const stream = tsProject.src().pipe(tsProject());
 
 gulp.task("clean-up", function () {
   return del("lib/**", { force: true });
 });
 
 gulp.task("build-typescript", function () {
-  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("lib"));
+  return merge([
+    stream.js.pipe(gulp.dest("lib/")),
+    stream.dts.pipe(gulp.dest("lib/"))
+  ]);
 });
 
 gulp.task("default", gulp.series("clean-up", "build-typescript"));
